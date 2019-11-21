59978fd1e19ec826b640ca06c9aaa6df7c88e324
Maria Ronauli <mronauli@gmail.com>
2019-11-21 15:40:54 -0700

    Add squared numbers

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -14,16 +14,29 @@ divide_sum_of_ages_by_number_of_ages  = sum.to_f/count_number_of_ages.to_f
 puts divide_sum_of_ages_by_number_of_ages
 
 
-  ages = [24, 30, 18, 20, 41]
+
 
   numbers = []
 
   ages.each do |ages|
 
   numbers << ages - divide_sum_of_ages_by_number_of_ages
+
   end
 
 puts numbers
+
+
+  squared_numbers = []
+
+
+  squared_numbers = numbers.map do |numbers|
+    numbers ** 2
+
+end
+
+puts squared_numbers
+
 
 
 # When you find the standard deviation, print it out
