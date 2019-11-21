d2cd4640800b63306b9c47fab89bc0bc2ad1db78
Maria Ronauli <mronauli@gmail.com>
2019-11-21 14:50:25 -0700

    Subtract integers by average

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -13,4 +13,18 @@ divide_sum_of_ages_by_number_of_ages  = sum.to_f/count_number_of_ages.to_f
 
 puts divide_sum_of_ages_by_number_of_ages
 
+
+  ages = [24, 30, 18, 20, 41]
+
+  numbers = []
+
+  ages.each do |ages|
+
+  numbers << ages - divide_sum_of_ages_by_number_of_ages
+  end
+
+
+puts numbers.round
+
+
 # When you find the standard deviation, print it out
