d02b8de7a4b135b0cae8fa78641bb0f31a249002
Maria Ronauli <mronauli@gmail.com>
2019-11-21 14:06:52 -0700

    Make divide_sum_of_ages_by_number_of_ages a decimal

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -8,4 +8,9 @@ puts sum
 count_number_of_ages = ages.count()
 
 puts count_number_of_ages
+
+divide_sum_of_ages_by_number_of_ages  = sum.to_f/count_number_of_ages.to_f
+
+puts divide_sum_of_ages_by_number_of_ages
+
 # When you find the standard deviation, print it out
